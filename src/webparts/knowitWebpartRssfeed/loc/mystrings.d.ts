5f30d9ce8e7daeefcb450edeb181ac9d2c8bbd41
MoltoMix <ole.akre@knowit.no>
2022-05-19 11:46:33 +0200

    First draft

---
+++ src/webparts/knowitWebpartRssfeed/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IKnowitWebpartRssfeedWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'KnowitWebpartRssfeedWebPartStrings' {
+  const strings: IKnowitWebpartRssfeedWebPartStrings;
+  export = strings;
+}
